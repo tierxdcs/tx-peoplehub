6e52cdce898212fc9c593f2f4aa78977c4951077
ngangadhar-pki <nithin.gangadhar@perkinelmer.com>
2026-01-05 13:48:36 -0500

    Add Admin tab data entry flow

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PeopleProfileComponent } from './people-profile/people-profile.component';
+import { AdminComponent } from './admin/admin.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'people/:id', component: PeopleProfileComponent },
+  { path: 'admin', component: AdminComponent },
   { path: '**', redirectTo: '' }
 ];
