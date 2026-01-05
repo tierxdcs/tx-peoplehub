412245605243ebf04d1c949a51f16cbb4ed4b1d1
ngangadhar-pki <nithin.gangadhar@perkinelmer.com>
2026-01-05 14:09:36 -0500

    Add scorecard route and link

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PeopleProfileComponent } from './people-profile/people-profile.component';
 import { AdminComponent } from './admin/admin.component';
+import { ScorecardComponent } from './scorecard/scorecard.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'people/:id', component: PeopleProfileComponent },
+  { path: 'people/:id/scorecard', component: ScorecardComponent },
   { path: 'admin', component: AdminComponent },
   { path: '**', redirectTo: '' }
 ];
