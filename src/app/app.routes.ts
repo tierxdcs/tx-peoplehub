0d4e5aec5eb7ef168f6ea65ae44a71ea8e4833c2
ngangadhar-pki <nithin.gangadhar@perkinelmer.com>
2026-01-05 13:37:40 -0500

    Add profile route and activate View profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PeopleProfileComponent } from './people-profile/people-profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'people/:id', component: PeopleProfileComponent },
+  { path: '**', redirectTo: '' }
+];
