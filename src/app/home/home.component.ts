7f8345fbb1454a7f05616e9852337b778fb9f91d
ngangadhar-pki <nithin.gangadhar@perkinelmer.com>
2026-01-05 13:40:32 -0500

    Fix profile routing build errors

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
