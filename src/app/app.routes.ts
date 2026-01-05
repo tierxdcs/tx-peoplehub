b2b2e03963206ab5e5fef2310844f0aab47d6dd2
ngangadhar-pki <nithin.gangadhar@perkinelmer.com>
2026-01-05 14:50:52 -0500

    Activate Teams tab

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { HomeComponent } from './home/home.component';
 import { PeopleProfileComponent } from './people-profile/people-profile.component';
 import { AdminComponent } from './admin/admin.component';
 import { ScorecardComponent } from './scorecard/scorecard.component';
+import { TeamsComponent } from './teams/teams.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'teams', component: TeamsComponent },
   { path: 'people/:id', component: PeopleProfileComponent },
   { path: 'people/:id/scorecard', component: ScorecardComponent },
   { path: 'admin', component: AdminComponent },
