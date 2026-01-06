7b3d45046cdb018a0e26fe48c78e53b525256391
ngangadhar-pki <nithin.gangadhar@perkinelmer.com>
2026-01-06 05:18:16 -0500

    Show leave request confirmation status

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {}
+export class HomeComponent {
+  leaveForm = {
+    type: 'PTO',
+    startDate: '',
+    endDate: '',
+    notes: ''
+  };
+  leaveStatus = '';
+
+  submitLeave() {
+    if (!this.leaveForm.startDate || !this.leaveForm.endDate) {
+      this.leaveStatus = 'Please select start and end dates.';
+      return;
+    }
+
+    this.leaveStatus = 'Pending manager approval';
+  }
+}
