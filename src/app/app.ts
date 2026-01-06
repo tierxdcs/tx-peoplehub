88c746d3dc6ad5275a4b1428f1029a5da9fe5051
ngangadhar-pki <nithin.gangadhar@perkinelmer.com>
2026-01-06 14:38:24 -0500

    Hide chrome on login

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +10,17 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 })
 export class App {
   notificationsOpen = false;
+  showChrome = true;
+
+  constructor(private readonly router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.showChrome = !event.urlAfterRedirects.startsWith('/login');
+      }
+    });
+  }
 
   toggleNotifications() {
     this.notificationsOpen = !this.notificationsOpen;
