76fcdb31c2c8734bda526f58d8cdb88f7c8d06a6
ngangadhar-pki <nithin.gangadhar@perkinelmer.com>
2026-01-06 05:23:57 -0500

    Revert "Show leave request confirmation status"
    This reverts commit d7d8b8056cdd8096f2f882e4696782b762e620db.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,29 +1,11 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, FormsModule],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-  leaveForm = {
-    type: 'PTO',
-    startDate: '',
-    endDate: '',
-    notes: ''
-  };
-  leaveStatus = '';
-
-  submitLeave() {
-    if (!this.leaveForm.startDate || !this.leaveForm.endDate) {
-      this.leaveStatus = 'Please select start and end dates.';
-      return;
-    }
-
-    this.leaveStatus = 'Pending manager approval';
-  }
-}
+export class HomeComponent {}
