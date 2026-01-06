35ad8543d41bd698f56fdc55796916d4882ad72c
ngangadhar-pki <nithin.gangadhar@perkinelmer.com>
2026-01-06 14:36:20 -0500

    Add enterprise login screen

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [RouterLink, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  credentials = {
+    email: '',
+    password: '',
+    remember: true
+  };
+}
