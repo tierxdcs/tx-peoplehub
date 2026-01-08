22bf5a2d957d5a775fd2002292fcaa6ca12c5d6d
ngangadhar-pki <nithin.gangadhar@perkinelmer.com>
2026-01-08 13:25:45 -0500

    Increase API payload limit for profile photos

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,8 @@ const { getPool, secrets } = require('./db');
 
 const app = express();
 app.use(cors({ origin: true }));
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 
 const getPoolInstance = () => getPool();
 
